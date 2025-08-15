b5126966418cba379b111fb8bc39579d0e182e27
Syeda Humerah Ahmad Shah <humawork88@gmail.com>
2025-08-15 16:14:05 +0500

    Add basics.py program

---
+++ basics.py
@@ -0,0 +1,14 @@
+name = "Huma"
+age = 22
+height = 5.6
+is_student = False
+
+print("Name: ", name)
+print("Age: ", age)
+print("Height: ", height)
+print("Is Student: ", is_student)
+
+user_name = input("Enter your Name: ")
+user_age = int(input("Enter your Age: "))
+
+print(f"Hello {user_name}. \nYou are {user_age} years old.")
