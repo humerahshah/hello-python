f396452c45cf9d1581e90c645158b4f8a48e93c4
Syeda Humerah Ahmad Shah <humawork88@gmail.com>
2025-08-16 16:38:36 +0500

    simple calculator

---
+++ calculator.py
@@ -0,0 +1,7 @@
+a = int (input("Enter 1st number: "))
+b = int (input("Enter 2nd number: "))
+
+print(f"Sum: {a+b}")
+print(f"Difference: {a-b}")
+print(f"Product: {a*b}")
+print(f"Quotient: {a/b}")
